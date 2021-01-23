fdb72a881ab1efa1f3a86e77282c9409736ec152
Michelle Ho <michelle.ho@intel.com>
2021-01-22 17:14:33 -0800

    Add function to check if old and new files are the same

--- add_timestamp.sh
+++ add_timestamp.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+function check_file {
+
+  if cmp --silent -- "$1" "$2"; then
+    echo "$1 and $2 contents are identical"
+  else
+    echo "$1 and $2 differ"
+  fi
+
+}
+
 current_date=$(date +"%m_%d_%y")
 timestamped_files="files_${current_date}"
 
@@ -10,5 +20,7 @@ all_files=`ls *.sh `
 for file in $all_files; do
   name="${file%.*}"
   ext="${file##*.}"
-  cp -v $file $timestamped_files/$name.$current_date.$ext
+  new_file=$timestamped_files/$name.$current_date.$ext
+  cp -v $file $new_file
+  check_file $file $new_file	
 done;

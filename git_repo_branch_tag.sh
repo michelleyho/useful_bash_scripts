7ced1a2441055dd6a54743b4b8438af67eb6c2a2
Michelle Ho <michelle.y.ho@gmail.com>
2021-03-17 17:43:48 -0700

    Added user help features
    - create help menu option.
    - create error handling of when new tag already exists
    - create error handling when new branch already exists

--- git_repo_branch_tag.sh
+++ git_repo_branch_tag.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
-set -e
+#set -e
+
+########
+# Help
+########
+Help()
+{
+    # Display Help
+    echo "Welcome to the git branch and release automation bash script"
+    echo ""
+    echo "To run this script:"
+    echo ">> ./git_repo_branch_tag.sh <ssh repo url> <source branch> <new branch name> <new tag name>"
+    echo ""
+    echo "Options:"
+    echo "h --> Print this help menu"
+
+}
+
+while getopts ":h" option; do
+    case $option in
+        h) # display help
+            Help
+            exit;;
+        \?) # invalid option
+            echo "Please enter another option"
+            exit;;
+    esac
+done
+
 
 repo=$1
 source_branch=$2
@@ -12,10 +40,28 @@ echo "Will be performing branching and releasing on repo:${repo} on branch ${sou
 echo "New branch name: ${branch_name}"
 echo "New tag name: ${tag_name}"
 
+failCount=0
+
 git clone ${repo}
 cd ${repo_name}
 git checkout ${source_branch}
-git checkout -b ${branch_name}
-git push origin ${branch_name}
-git tag ${tag_name}
-git push origin --tags
+git ls-remote -h --exit-code ${repo} ${branch_name}
+if [ $? == 2 ]; then
+    git checkout -b ${branch_name}
+#    git push origin ${branch_name}
+else
+    echo "New branch name ${branch_name} already exists in remote repo"
+    exit
+fi
+
+tag_error_message=$( git tag ${tag_name} 2>&1 )
+if [ $? == 0 ]; then
+    git push origin --tags
+    echo "Successfully tagged"
+else
+    echo "Failed to create tag: ${tag_name}"
+    echo ${tag_error_message}
+    let "failCount += 1"
+fi
+
+echo "Fail: ${failCount}"

c9b8f9de48a722707071c4890c25811266d96bbd
Michelle Ho <michelle.y.ho@gmail.com>
2021-03-17 20:41:33 -0700

    Refactored code
    Separate into functions that handle error situations

--- git_repo_branch_tag.sh
+++ git_repo_branch_tag.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #set -e
 
-########
-# Help
-########
+#############################################
+# Help Menu
+#############################################
 Help()
 {
     # Display Help
@@ -17,6 +17,54 @@ Help()
 
 }
 
+function checkout_repo
+{
+    if ! (git clone ${repo}) then
+        echo >&2 "Fail to clone repo"
+        exit 1
+    else
+        cd ${repo_name}
+        git checkout ${source_branch} || { echo >&2 "Failed to checkout branch"; exit 1; }
+        echo "Successfully cloned and checkout branch"
+    fi
+}
+
+function create_new_branch
+{
+    git ls-remote -h --exit-code ${repo} ${branch_name}
+    if [ $? == 2 ]; then
+        git checkout -b ${branch_name}
+        git push origin ${branch_name}
+        echo "Succesfully created new branch ${branch_name}"
+    else
+        echo "New branch name ${branch_name} already exists in remote repo"
+        exit
+    fi
+}
+
+function create_new_tag
+{
+    git ls-remote -t --exit-code ${repo} ${tag_name}
+    if [ $? == 2 ]; then
+        tag_error_message=$( git tag ${tag_name} 2>&1 )
+        if [ $? == 0 ]; then
+            git push origin --tags
+            echo "Successfully tagged"
+        else
+            echo "Failed to create tag: ${tag_name}"
+            echo ${tag_error_message}
+            let "failCount += 1"
+        fi
+    else
+        echo "New tag name ${tag_name} already exists in remote repo"
+        exit
+    fi
+}
+
+
+#############################################
+# Main Program
+#############################################
 while getopts ":h" option; do
     case $option in
         h) # display help
@@ -28,7 +76,6 @@ while getopts ":h" option; do
     esac
 done
 
-
 repo=$1
 source_branch=$2
 branch_name=$3
@@ -36,32 +83,15 @@ tag_name=$4
 
 basename=$(basename $repo)
 repo_name=${basename%.*}
+failCount=0
+
 echo "Will be performing branching and releasing on repo:${repo} on branch ${source_branch}"
 echo "New branch name: ${branch_name}"
 echo "New tag name: ${tag_name}"
 
-failCount=0
 
-git clone ${repo}
-cd ${repo_name}
-git checkout ${source_branch}
-git ls-remote -h --exit-code ${repo} ${branch_name}
-if [ $? == 2 ]; then
-    git checkout -b ${branch_name}
-#    git push origin ${branch_name}
-else
-    echo "New branch name ${branch_name} already exists in remote repo"
-    exit
-fi
-
-tag_error_message=$( git tag ${tag_name} 2>&1 )
-if [ $? == 0 ]; then
-    git push origin --tags
-    echo "Successfully tagged"
-else
-    echo "Failed to create tag: ${tag_name}"
-    echo ${tag_error_message}
-    let "failCount += 1"
-fi
+checkout_repo
+create_new_branch
+create_new_tag
 
 echo "Fail: ${failCount}"
